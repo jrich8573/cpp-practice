1cbc751e59a61231ffd00bcb1d7d9ff08cf51cfe
Jason Rich <jasonrich85@gmail.com>
2017-08-12 18:36:08 -0400

    added files

--- variables/variables.cpp
+++ variables/variables.cpp
@@ -7,9 +7,22 @@ using namespace std;
 
 int main(){
 
-    int a = 4;
+    // 4 byte int vars range is -2B to 2B
+    // 2 byte short int vars range is -32768 to 32767
+
+    int a = 4; // 4bytes
+    short b = 5; // 2bytes truncates the decimal
+    float c = 5.3; // 4 bytes
+    double d = 5.4; // 8 bytes more precise than float
+    unsigned int e = 58;
+    const string NAME = "Batman"; // convention when using constants is to use uppercase
     cout << a << '\n';
     cout << &a << '\n'; //&a is the address of a
-    
+    cout << b << '\n';
+    cout << c << '\n';
+    cout << d << '\n';
+    cout << e << '\n';
+    cout << NAME << '\n';
+
     return 0;
 }
