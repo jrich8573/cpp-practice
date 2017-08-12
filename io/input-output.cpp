c5ec3974ff0e74832744413acd10f85f7e25b997
Jason Rich <jasonrich85@gmail.com>
2017-08-12 19:06:10 -0400

    fixed formatting

---
+++ io/input-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+
+    cout << "Enter the vaule of a" << '\n';
+    cin >>  a;//console in
+
+    cout << "the value of variable a is " << a << '\n';
+
+    return 0;
+}
