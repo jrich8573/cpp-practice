3dd87a346daf8f749262c21cfcb3ffe879540a09
Jason Rich <jasonrich85@gmail.com>
2017-08-12 20:23:35 -0400

    added comments in main function.

--- io/input-output.cpp
+++ io/input-output.cpp
@@ -7,7 +7,8 @@ int main(){
     int a;
 
     cout << "Enter the vaule of a" << '\n';
-    cin >>  a;//console in
+    cin >>  a; //console in: accepts user input, and stores the value, as
+               // long as the input value is the same data type. 
 
     cout << "the value of variable a is " << a << '\n';
 
